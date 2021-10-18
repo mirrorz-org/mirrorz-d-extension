9a368d563cd19ed2cf23c43ae88ad4d53c14203e
Kaijie Chen <chen@kaijie.org>
2021-10-18 21:14:38 +0800

    Add xidian (#3)
    * Add xidian
    
    * xidian.json: remove IPv6

--- custom/index.js
+++ custom/index.js
@@ -7,4 +7,5 @@ module.exports = {
   "neo"  : async (e) => file("tuna", e),
   "hit"  : async (e) => file("hit",  e),
   "bjtu"  : async (e) => file("bjtu",  e),
+  "xidian"  : async (e) => file("xidian",  e),
 };
