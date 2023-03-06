5176a5563273b02165d385c5b04e5d483d8d7909
Hagb <hagb@hagb.name>
2023-03-07 00:04:37 +0800

    Add cqu

--- custom/index.js
+++ custom/index.js
@@ -9,5 +9,6 @@ module.exports = {
   "neo"  : async (e) => file("tuna", e),
   "hit"  : async (e) => file("hit",  e),
   "bjtu" : async (e) => file("bjtu",  e),
+  "cqu"  : async (e) => file("cqu",  e),
   "xjtu" : xjtu,
 };
