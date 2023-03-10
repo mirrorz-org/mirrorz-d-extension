3ef98bff9613e48adb22c2130fbd7aa628244e6a
Aethoce <Lijinghong0233@gmail.com>
2023-03-10 11:14:56 +0800

    Update index.js

--- custom/index.js
+++ custom/index.js
@@ -9,7 +9,7 @@ module.exports = {
   "neo"  : async (e) => file("tuna", e),
   "hit"  : async (e) => file("hit",  e),
   "bjtu" : async (e) => file("bjtu",  e),
-  "cqu": async (e) => file("cqu", e),
-  "qlut"  : async (e) => file("qlut",  e),
+  "cqu"  : async (e) => file("cqu", e),
+  "qlut" : async (e) => file("qlut",  e),
   "xjtu" : xjtu,
 };
